7c2c7ffe108f662c85f05a2b670f84f516afa0db
Aditya Agarwal <Aditya@Adityas-MacBook-Pro-3.local>
2018-02-15 23:18:10 +0800

    Updated the user guide

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -10,14 +10,14 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Keyword matching is not case sensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "the specified keywords (not case-sensitive) and displays them as a list with index numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
@@ -50,11 +50,20 @@ public class FindCommand extends Command {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
+            final ArrayList<String> wordsInLowerCase = getArrayListInLowerCase(wordsInName);
+            final ArrayList<String> keywordsInLowerCase = getArrayListLowerCase(keywords);
             if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
             }
         }
         return matchedPersons;
     }
+
+    /**
+     * An ArrayList is retrieved 
+     *
+     * @param keywords for searching
+     * @return list of persons found
+     */
 
 }
